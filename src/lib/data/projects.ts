f6ec7c2cece23e375b63d881205b353d06574d91
Jude Lobo <judejonathan.lobo@gmail.com>
2024-05-06 19:40:49 -0400

    edited projects.ts file

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -99,6 +99,54 @@ export const items: Array<Project> = [
 		skills: getSkills('php', 'js', 'codeigniter', 'mysql', 'css', 'html'),
 		type: 'In-house Project',
 	},
+	{
+		slug: 'jazz',
+		color: '#ff3e00',
+		description:
+			"<p>Jazz's website is a portal for telecom services, offering mobile plans, internet packages, and digital solutions. It provides users with access to account management tools and customer support.</p><h4>Responsibilities:</h4><ul><li>Managed backend portal and synchronized website.</li><li>Created and managed APIs for user transactions.</li></ul>",
+		shortDescription:
+			"<p>Jazz's website is a portal for telecom services, offering mobile plans, internet packages, and digital solutions. It provides users with access to account management tools and customer support.</p>",
+		links: [],
+		logo: Assets.Jazz,
+		name: 'Jazz',
+		period: {
+			from: new Date()
+		},
+		skills: getSkills('php', 'js', 'codeigniter', 'mysql', 'css', 'html'),
+		type: 'In-house Project',
+	},
+	{
+		slug: 'knorr',
+		color: '#ff3e00',
+		description:
+			"<p>Knorr's website features recipes, cooking tips, and product information related to Knorr's food products. It offers a platform for users to discover new recipes and engage with the Knorr brand.</p><h4>Responsibilities:</h4><ul><li>Designed and implemented various website features and modules.</li><li>Enhanced user engagement and brand visibility.</li></ul>",
+		shortDescription:
+			"<p>Knorr's website features recipes, cooking tips, and product information related to Knorr's food products. It offers a platform for users to discover new recipes and engage with the Knorr brand.</p>",
+		links: [],
+		logo: Assets.Knorr,
+		name: 'Knorr',
+		period: {
+			from: new Date()
+		},
+		skills: getSkills('php', 'js', 'codeigniter', 'mysql', 'css', 'html'),
+		type: 'In-house Project',
+	},
+	{
+		slug: 'lipton',
+		color: '#ff3e00',
+		description:
+			"<p>Lipton's website is focused on tea products, offering information about Lipton's tea varieties, recipes, and health benefits. It provides users with resources to explore tea-related content and promotions.</p><h4>Responsibilities:</h4><ul><li>Utilized AEM to create and integrate multiple modules.</li><li>Collaborated with design and marketing teams.</li></ul>",
+		shortDescription:
+			"<p>Lipton's website is focused on tea products, offering information about Lipton's tea varieties, recipes, and health benefits. It provides users with resources to explore tea-related content and promotions.</p>",
+		links: [],
+		logo: Assets.Lipton,
+		name: 'Lipton',
+		period: {
+			from: new Date()
+		},
+		skills: getSkills('php', 'js', 'codeigniter', 'mysql', 'css', 'html'),
+		type: 'In-house Project',
+	},
 ];
 
 export const title = 'Projects';

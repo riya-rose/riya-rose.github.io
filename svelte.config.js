46fb5bef26ad57e5e5cbc98c1c75c459256487c4
Jude Lobo <judejonathan.lobo@gmail.com>
2024-05-06 11:41:48 -0400

    Modified svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/jude-lobo.github.io';
+const base = '/jude-lobo';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {

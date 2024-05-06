9a39c984b43713cc9848c46244e0450f3cd0942e
Jude Lobo <judejonathan.lobo@gmail.com>
2024-05-06 10:20:11 -0400

    Added theme

---
+++ src/lib/data/education.ts
@@ -0,0 +1,31 @@
+import Assets from './assets';
+import type { Education } from '../types';
+
+export const items: Array<Education> = [
+	{
+		degree: 'Post Graduate Diploma',
+		description: '',
+		location: 'Pakistan',
+		logo: Assets.VUni,
+		name: '',
+		organization: 'Virtual University of Pakistan',
+		period: { from: new Date(2021, 3, 1), to: new Date(2022, 3, 1) },
+		shortDescription: '',
+		slug: 'virtual-university',
+		subjects: []
+	},
+	{
+		degree: 'Bachelors in Software Engineering',
+		description: '',
+		location: 'Pakistan',
+		logo: Assets.Unknown,
+		name: '',
+		organization: 'IQRA University',
+		period: { from: new Date(2015, 4, 1), to: new Date(2019, 11, 1) },
+		shortDescription: '',
+		slug: 'iqra-university',
+		subjects: []
+	},
+];
+
+export const title = 'Education';
---
+++ src/lib/data/experience.ts
@@ -0,0 +1,38 @@
+import Assets from './assets';
+import { getSkills } from './skills';
+import { ContractType, type Experience } from '../types';
+
+export const items: Array<Experience> = [
+	{
+		slug: 'backend-developer',
+		company: 'Symmetry Digital',
+		description: 'As a PHP Developer at Symmetry Digital, I developed and integrated efficient PHP modules for web application back-end functionality. Leveraging JavaScript and jQuery, I created engaging user experiences. Managed MySQL databases for data storage and functionality. Contributed to research for technology improvements. Conducted troubleshooting for optimal performance. Monitored site performance, collaborated with front-end teams, and enhanced features for better user experience.',
+		contract: ContractType.FullTime,
+		type: 'Web Development',
+		location: 'Karachi, Pakistan',
+		period: { from: new Date(2020, 0, 29), to: new Date(2024, 1, 5) },
+		skills: getSkills('php', 'js', 'codeigniter', 'laravel', 'mysql', 'css', 'html'),
+		name: 'Senior Backend Developer',
+		color: 'red',
+		links: [],
+		logo: Assets.Symmetry,
+		shortDescription: 'As a PHP Developer at Symmetry Digital, I developed and integrated efficient PHP modules for web application back-end functionality. Leveraging JavaScript and jQuery, I created engaging user experiences. Managed MySQL databases for data storage and functionality. Contributed to research for technology improvements. Conducted troubleshooting for optimal performance. Monitored site performance, collaborated with front-end teams, and enhanced features for better user experience.'
+	},
+	{
+		slug: 'backend-developer',
+		company: 'Horizon Technologies',
+		description: 'At Horizon Technologies, during my internship as a Backend Web Developer, I optimized PHP modules, integrated RESTful APIs into Laravel applications, and conducted code reviews to improve performance. Utilizing JavaScript and jQuery, I crafted engaging user experiences and ensured streamlined data management with MySQL. I diagnosed and resolved bugs, managed version control with Git, and created comprehensive documentation for seamless knowledge transfer.',
+		contract: ContractType.Internship,
+		type: 'Web Development',
+		location: 'Karachi, Pakistan',
+		period: { from: new Date(2019, 8, 2), to: new Date(2020, 0, 1)},
+		skills: getSkills('php', 'js', 'laravel', 'mysql', 'css', 'html'),
+		name: 'Junior Backend Developer',
+		color: 'blue',
+		links: [],
+		logo: Assets.Horizon,
+		shortDescription: 'At Horizon Technologies, during my internship as a Backend Web Developer, I optimized PHP modules, integrated RESTful APIs into Laravel applications, and conducted code reviews to improve performance. Utilizing JavaScript and jQuery, I crafted engaging user experiences and ensured streamlined data management with MySQL. I diagnosed and resolved bugs, managed version control with Git, and created comprehensive documentation for seamless knowledge transfer.'
+	},
+];
+
+export const title = 'Experience';

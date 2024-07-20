1221f5c53603cd18a5d8ab11a2232e5af71ce4ff
riya-rose <riyarose71@yahoo.com>
2024-07-20 02:54:33 -0400

    Added Favicon, changed resume, and changed uni logos

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,3 +1,3 @@
-export const data = 'https://drive.google.com/file/d/1oSkBQ9SiS3uT24sT65auwW-wSxg_O7rz/view?usp=drive_link';
+export const data = 'https://drive.google.com/file/d/1ON1UcIg4AGFUBqpV3A-aFtdsY_SiYX6l/view?usp=drive_link';
 
 export const title = 'Resumé';

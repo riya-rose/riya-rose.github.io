bcec14d4af68ac2f7cd66d51630ca71a1a34d2b1
riya-rose <riyarose71@yahoo.com>
2024-08-14 16:38:11 -0400

    added sqeemshots

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -20,27 +20,27 @@ export const items: Array<Project> = [
 		screenshots: [
 			{
 				label: 'screen 1',
-				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+				src: 'https://drive.google.com/file/d/1BSoRtH3CXqNuULzeP1Efylr6G9HW06_J/view?usp=drive_link'
 			},
 			{
 				label: '2',
-				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+				src: 'https://drive.google.com/file/d/1mzZYfrTcPumF2DmsUaZHQX_saPOXS90e/view?usp=drive_link'
 			},
 			{
 				label: '3',
-				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+				src: 'https://drive.google.com/file/d/1pIr-98mHzOIYqh-6vJ1sIJNIZnYhaMDR/view?usp=drive_link'
 			},
 			{
 				label: '4',
-				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+				src: 'https://drive.google.com/file/d/1w9EwzTTpTvnLvznP34ectZyWQUuGitSy/view?usp=drive_link'
 			},
 			{
 				label: '5',
-				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+				src: 'https://drive.google.com/file/d/1az0Gq2ucNHUeXVpdOQE68cBCL6Ky9KhI/view?usp=drive_link'
 			},
 			{
 				label: '6',
-				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+				src: 'https://drive.google.com/file/d/1CqtXB_IVJ8TbiNNbTLiQj8p_hp9CXfEv/view?usp=drive_lin'
 			},
 		],
 	},

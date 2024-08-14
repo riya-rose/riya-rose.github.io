fa1e711ba6b0f90e0d174a347bb375ed8342db06
riya-rose <riyarose71@yahoo.com>
2024-08-14 21:51:12 +0000

    Deploying to gh-pages from @ riya-rose/riya-rose.github.io@639f024e13cb0e169686fe6967ed99a51c0be23d 🚀

---
+++ _app/immutable/chunks/stores.b69c4fc9.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.e203e60f.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};

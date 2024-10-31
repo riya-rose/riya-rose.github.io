f489e588c40da548d16b9242db5e4a364456b4fd
jude-lobo <judejonathan.lobo@gmail.com>
2024-10-31 19:23:34 -0400

    Added loader

---
+++ src/JudeLoader.svelte
@@ -0,0 +1,46 @@
+<!-- src/Loader.svelte -->
+<script>
+	export let visible = true;
+</script>
+
+{#if visible}
+	<div class="loader">
+		<div class="spinner" />
+	</div>
+{/if}
+
+<style>
+	/* Center the loader */
+	.loader {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: rgba(255, 255, 255, 0); /* Slightly transparent background */
+		z-index: 1000;
+	}
+
+	/* Create the spinner */
+	.spinner {
+		width: 50px;
+		height: 50px;
+		border: 5px solid #f3f3f3; /* Light grey */
+		border-top: 5px solid #f03b31; /* Blue color */
+		border-radius: 50%;
+		animation: spin 1s linear infinite;
+	}
+
+	/* Keyframes for the spin animation */
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
